532da97525c66fab162de612a94ae02f6d2f4e11
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2021-06-04 10:27:14 +0100

    Update copyright header

--- src/main/kotlin/dev/cbyrne/kotify/api/section/user/KotifyAPIUserSection.kt
+++ src/main/kotlin/dev/cbyrne/kotify/api/section/user/KotifyAPIUserSection.kt
@@ -1,18 +1,19 @@
 /*
- * Copyright (c) 2021 dreamhopping <https://github.com/dreamhopping>
+ *     Kotify is a lightweight and modern Kotlin API Wrapper for Spotify.
+ *     Copyright (C) 2021  Conor Byrne <https://github.com/cbyrneee>
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package dev.cbyrne.kotify.api.section.user
